3d92dbdb122fb5f98d5c0225659cfb198a6d61f9
AgneseBettio <bettio.agnese@gmail.com>
2021-05-11 15:37:07 +0200

    inizializzazione progetto

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// Attraverso l'apposita API di Boolean generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+// https://flynn.boolean.careers/exercises/api/random/mail
+
+//collegamento a vue
+
+const app = new Vue({
+    el: '#root',
+
+    data : {
+
+    },
+    methods : {
+        
+    },
+    mounted : {
+
+    },
+
+})
